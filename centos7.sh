e83a556bd67bf1f81f68a19f0002784a6610ad3d
Nahid Hasan Nirob <46191023+Nirob3x@users.noreply.github.com>
2019-12-20 00:44:33 +0800

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -10,11 +10,11 @@ echo " OpenVPN Access Server With Unlimited License BY Nirob3x"
 yum update -y
 yum install wget -y
 yum install net-tools -y
-wget https://pswfbd.org/files1/openvpn-as-2.5-CentOS7.x86_64.rpm
+wget https://zerohost.co/f/openvpn-as-2.5-CentOS7.x86_64.rpm
 rpm -i openvpn-as-2.5-CentOS7.x86_64.rpm
 cd /usr/local/openvpn_as/lib/python2.7/site-packages/
 rm pyovpn-2.0-py2.7.egg
-wget https://pswfbd.org/files1/pyovpn-2.0-py2.7.egg
+wget https://zerohost.co/f/pyovpn-2.0-py2.7.egg
 cd /usr/local/openvpn_as/bin
 ./ovpn-init
 
